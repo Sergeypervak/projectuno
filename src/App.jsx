4e1a0ff241fff647df44e13f799bf2392cd74cac
Sergeypervak <har4enko1983@gmail.com>
2022-06-09 22:07:44 +0300

    optimal kind of setState

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,18 @@ class App extends Component {
 }
 
 handleClick = () => {
-  this.setState({count: this.state.count + 1})
+  this.setState({count: this.state.count + 1});
+  // this.setState((prevState)=>({count: prevState.count + 1}))
+  // this.setState((prevState)=>({count: prevState.count + 1}))
+
+   console.log('from handle click');
+
+
 }
 render () {
     return (
       <div className="App">
-        <h2>counter</h2>
+        <h2>count</h2>
       
       <button onClick={this.handleClick}>{this.state.count}</button>
       </div>
