d7148133f3dd0287326308445832cc78cfab349b
Sergeypervak <har4enko1983@gmail.com>
2022-06-12 22:43:30 +0300

    add array of posts

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
-import React from "react";
+import React, {Component} from "react";
 
 
 class App extends Component {
 state = {
+  posts: [
+    {id: 'abc1', name: 'Junior'},
+    {id: 'abc2', name: 'Middle'},
+    {id: 'abc3', name: 'Senjor'},
+
+
+  ]
    
-}
+};
 
 
 render () {
     return (
-      <div className="App"></div>
+      <div className="App">
+         <h2>{this.state.posts[0].name}</h2>
+         <h2>{this.state.posts[1].name}</h2>
+         <h2>{this.state.posts[2].name}</h2>
+
+     </div>
   );
      } ;
      
