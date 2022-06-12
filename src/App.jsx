e7462aa49694926d8e5976f03b3af3e982b13114
Sergeypervak <har4enko1983@gmail.com>
2022-06-12 21:47:52 +0300

    working with collections and 'key' atributes

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,18 @@
 import React from "react";
 
 
-export default class App extends React.Component {
+class App extends Component {
 state = {
-    count: 0,
-    isCounting: false
-  };
-componentDidMount() {
-const userCount = localStorage.getItem('timer');
-if (userCount) {
-  this.setState({count: +userCount})
-}
+   
 }
 
-componentDidUpdate() {
-localStorage.setItem('timer', this.state.count)
-}
-componentWillUnmount() {
-  clearInterval(this.counterId);
-}
 
-handleStart = () => {
-  this.setState({isCounting: true})
- 
-  this.counterId = setInterval(()=>{
-    this.setState({count: this.state.count + 1})
-  },1000)
-}
-
-handleStop = () => {
-  this.setState({isCounting: false})
-  clearInterval(this.counterId);
-} 
-
-handleReset =() => {
-  this.setState({isCounting: false, count: 0})
-  clearInterval(this.counterId);
-}
 render () {
     return (
-      <div className="App">
-     <h1>React timer</h1>
-     <h3>{this.state.count}</h3>
-     {!this.state.isCounting ? (
-     <button onClick={this.handleStart}>start</button>) :
-     <button onClick={this.handleStop}>stop</button> };
-     <button onClick={this.handleReset}>Reset</button> 
-      </div>
-    );
-     } 
+      <div className="App"></div>
+  );
+     } ;
      
-}
+    }
 
-    
+  export default App;
