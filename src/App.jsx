c1bc9a5b7c0c7d8d6241e8a00c6c1c3d58bdc1d7
Sergeypervak <har4enko1983@gmail.com>
2022-06-14 23:27:46 +0300

    form.control components

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,13 @@
-import React, {Component} from "react";
-import {Posts} from './components/Posts'
+import {Form} from './components/Form'
 
-class App extends Component {
-state = {
-  posts: [
-    {id: 'abc1', name: 'Junior'},
-    {id: 'abc2', name: 'Middle'},
-    {id: 'abc3', name: 'Senjor'},
-],};
 
-deletePost = (id) => {
-  this.setState({posts: this.state.posts.filter(post => post.id !== id)})
-  
-}
-render () {
-  const {posts} = this.state
-  return (
-      <div style={{color: "yellow"}} className="App">
-       <Posts posts={posts} eraser={this.deletePost}/>
+function App() {
+return (
+  <div className="App">
+  <Form />
+  </div>
+)
 
-     </div>
-  );
-     } ;
-     
-    }
+};
 
-  export default App;
+export default App;
--- src/components/Post.jsx
+++
@@ -1,6 +0,0 @@
-export function Post (props)  {
-    const {id, name, eraser} = props
-    return <h2 >{name} <button style={{ color: "green"}}onClick= {() => eraser (id)}>delete</button></h2> 
-
-    
-}
