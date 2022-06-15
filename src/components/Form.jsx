f730cfa7b281d1c41a28fca72205d5d5f8158f4e
Sergeypervak <har4enko1983@gmail.com>
2022-06-15 22:46:07 +0300

    state in form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,11 +1,28 @@
 import React from "react";
 
 class Form extends React.Component {
+state = { 
+ firstName :'',
 
+}
+
+
+    handleChange = (event) => {
+        this.setState({firstName : event.target.value})
+    }
 
     render() {
-        return <div>
-            <input type="text"/>
+      const {firstName} = this.state;
+      
+      
+      return <div>
+            <input 
+            type="text" 
+            name='firstName'
+            placeholder="firstName"  
+            value={firstName} 
+            onChange={this.handleChange}
+        />    
         </div>
 
     }
