88f3bb1673c9568f5bddca6a2bfc9146de2ae268
Sergeypervak <har4enko1983@gmail.com>
2022-06-21 22:29:46 +0300

    onsubmit preventDefaultdone16===

--- src/components/UncontrolledForm.jsx
+++ src/components/UncontrolledForm.jsx
@@ -9,15 +9,20 @@ constructor() {
 }
 handleSubmit = (event) => {
 event.preventDefault();
-if(this.cardRef.current.value.length < 16){
+if(this.cardRef.current.value.length !== 16){
   alert('card number contain 16 symbols');
   return;
 }
+// if (this.cardRef.current.value.length === 16) {
+this.cardRef.current.value = '';
+this.emailRef.current.value = '';
+alert('your card number and email sent')
+// }
 }
 
-
-
 render() {
+      
+      
       
       return <form onSubmit={this.handleSubmit}>
           
